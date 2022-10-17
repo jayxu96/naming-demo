640b11aa98f0e0c8b0740a8892e50d66a5c0bf6f
jayxu96 <31394286+jayxu96@users.noreply.github.com>
2022-10-17 14:14:34 +0800

    Add v2 version

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jayxu96/naming-demo
 
 go 1.18
 
-require github.com/jayxu96/baby-naming v1.0.1 // indirect
+require (
+	github.com/jayxu96/baby-naming v1.0.1
+	github.com/jayxu96/baby-naming/v2 v2.0.1
+)
--- main.go
+++ main.go
@@ -4,15 +4,47 @@ import (
 	"fmt"
 
 	naming "github.com/jayxu96/baby-naming/naming"
+	naming2 "github.com/jayxu96/baby-naming/v2/naming"
 )
 
 func main() {
-    var lastName string;
-    fmt.Println("***** This is a Baby Naming Generator V1 *****")
-    fmt.Println("Please enter your Last Name: " )
+    var version int
+    fmt.Println("***** Welcome to Baby Naming Generator *****")
+    fmt.Println("Please select Generator version: 1,2,3")
+    fmt.Scanln(&version)
 
-    fmt.Scanln(&lastName)
-    fmt.Println("The baby name is " + naming.CreateBabyName(lastName))
+    var lastName string
+    var gender string
+    var male bool
+    if version == 1 {
+        fmt.Println("***** This is a Baby Naming Generator V1 *****")
+        fmt.Println("Please enter your Last Name: " )
 
+        fmt.Scanln(&lastName)
+        fmt.Println("The baby name is " + naming.CreateBabyName(lastName))
+    } else if version == 2 {
+//  ===================== v2 ========================
+
+        fmt.Println("***** This is a Baby Naming Generator *****")
+        fmt.Println("1. Please enter your Last Name: " )
+        fmt.Scanln(&lastName)
+        fmt.Println("2. Is your baby a boy or girl?")
+        fmt.Scanln(&gender)
+        fmt.Println("Your baby is a " + gender)
+        if gender == "boy" {
+           male = true
+        } else if gender == "girl" {
+          male = false
+        } else {
+          fmt.Println ("Sorry, this generator does not support generate names for other genders.")
+          return
+        }
+        fmt.Println("Congrats! The baby name is " + naming2.CreateBabyName(male, lastName))
+    } else if version == 3 {
+        fmt.Println("Congrats! The baby name is " + naming2.CreateBabyName(male, lastName))
+    } else {
+        fmt.Println ("Sorry, no such version")
+        return
+    }
 
 }
